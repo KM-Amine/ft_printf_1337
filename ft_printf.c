594788411208b5ef5983b0d2bb48000b9eb0a8ab
mkhellou <yoyomcawom@gmail.com>
2022-10-21 11:08:07 +0100

    version 2 of ft_printf good with the norm

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 09:06:03 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/10/21 10:56:20 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/10/21 11:07:23 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,26 @@ void	ft_putuns(unsigned int i, int *len)
 	ft_putchar(i % 10 + '0', len);
 }
 
+void	ft_parser(char c, va_list pntr, int *len)
+{
+	if (c == 'c')
+		ft_putchar(va_arg(pntr, int), len);
+	if (c == 's')
+		ft_putstr(va_arg(pntr, char *), len);
+	if (c == 'd' || c == 'i')
+		ft_putnbr(va_arg(pntr, int), len);
+	if (c == 'u')
+		ft_putuns(va_arg(pntr, unsigned int), len);
+	if (c == 'p')
+		ft_putaddr(va_arg(pntr, void *), HEX_LOW, len);
+	if (c == 'x')
+		ft_puthex(va_arg(pntr, unsigned int), HEX_LOW, len);
+	if (c == 'X')
+		ft_puthex(va_arg(pntr, unsigned int), HEX_UPP, len);
+	if (c == '%')
+		ft_putchar('%', len);
+}
+
 int	ft_printf(const char *s, ...)
 {
 	va_list	pntr;
@@ -49,36 +69,9 @@ int	ft_printf(const char *s, ...)
 			ft_putchar(s[i], &len);
 		}
 		else
-		{
-			i++;
-			if (s[i] == 'c')
-				ft_putchar(va_arg(pntr, int), &len);
-			if (s[i] == 's')
-				ft_putstr(va_arg(pntr, char *), &len);
-			if (s[i] == 'd' || s[i] == 'i')
-				ft_putnbr(va_arg(pntr, int), &len);
-			if (s[i] == 'u')
-				ft_putuns(va_arg(pntr, unsigned int), &len);
-			if (s[i] == 'p')
-				ft_putaddr(va_arg(pntr, void *), HEX_LOW, &len);
-			if (s[i] == 'x')
-				ft_puthex(va_arg(pntr, unsigned int), HEX_LOW, &len);
-			if (s[i] == 'X')
-				ft_puthex(va_arg(pntr, unsigned int), HEX_UPP, &len);
-			if (s[i] == '%')
-				ft_putchar('%', &len);
-		}
+			ft_parser(s[++i], pntr, &len);
 		i++;
 	}
 	va_end(pntr);
 	return (len);
-}
-
-int main (void)
-{
-	int	i;
-
-	i = 0;
-	printf("%d\n", printf("hello%c",'t'));
-	printf("%d", ft_printf("hello%c",'t'));
 }
