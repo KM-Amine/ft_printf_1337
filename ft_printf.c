201c7d25e80de0c25d1e4e5bc0dce649bfdc0245
mkhellou <yoyomcawom@gmail.com>
2022-10-21 10:57:37 +0100

    first version of the completed project

--- ft_printf.c
+++ ft_printf.c
@@ -6,23 +6,79 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 09:06:03 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/10/21 09:52:23 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/10/21 10:56:20 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
-// int	ft_printf(const char *s, ...);
-// {
-	
-// }
+
+void	ft_putsubaddr(unsigned long long i, char *base, int *len)
+{
+	if (i > ft_strlen(base)-1)
+		ft_putsubaddr(i / ft_strlen(base), base, len);
+	ft_putchar(base[i % ft_strlen(base)], len);
+}
+
+void	ft_putaddr(void *p, char *base, int *len)
+{
+	ft_putstr("0x", len);
+	ft_putsubaddr((unsigned long long)p, base, len);
+}
+
+void	ft_putuns(unsigned int i, int *len)
+{
+	if (i > 9)
+		ft_putuns(i / 10, len);
+	ft_putchar(i % 10 + '0', len);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	va_list	pntr;
+	size_t	i;
+	int		len;
+
+	i = 0;
+	len = 0;
+	va_start(pntr, s);
+	while (s[i])
+	{
+		if (s[i] != '%')
+		{
+			ft_putchar(s[i], &len);
+		}
+		else
+		{
+			i++;
+			if (s[i] == 'c')
+				ft_putchar(va_arg(pntr, int), &len);
+			if (s[i] == 's')
+				ft_putstr(va_arg(pntr, char *), &len);
+			if (s[i] == 'd' || s[i] == 'i')
+				ft_putnbr(va_arg(pntr, int), &len);
+			if (s[i] == 'u')
+				ft_putuns(va_arg(pntr, unsigned int), &len);
+			if (s[i] == 'p')
+				ft_putaddr(va_arg(pntr, void *), HEX_LOW, &len);
+			if (s[i] == 'x')
+				ft_puthex(va_arg(pntr, unsigned int), HEX_LOW, &len);
+			if (s[i] == 'X')
+				ft_puthex(va_arg(pntr, unsigned int), HEX_UPP, &len);
+			if (s[i] == '%')
+				ft_putchar('%', &len);
+		}
+		i++;
+	}
+	va_end(pntr);
+	return (len);
+}
 
 int main (void)
 {
 	int	i;
 
 	i = 0;
-	printf("%x\n",-1);
-	ft_puthex(-1,"0123456789abcdef",&i);
-	//ft_putnbr(12,&i);
+	printf("%d\n", printf("hello%c",'t'));
+	printf("%d", ft_printf("hello%c",'t'));
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,19 +6,25 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 08:48:25 by mkhellou          #+#    #+#             */
-/*   Updated: 2022/10/21 09:52:33 by mkhellou         ###   ########.fr       */
+/*   Updated: 2022/10/21 10:52:36 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <unistd.h>
+# include <stdarg.h>
+# define HEX_LOW "0123456789abcdef"
+# define HEX_UPP "0123456789ABCDEF"
 
 size_t	ft_strlen(char *s);
 void	ft_putchar(char c, int *len);
 void	ft_putstr(char *s, int *len);
 void	ft_putnbr(long i, int *len);
 void	ft_puthex(unsigned int i, char *base, int *len);
+void	ft_putsubaddr(unsigned long long i, char *base, int *len);
+void	ft_putaddr(void *p, char *base, int *len);
+void	ft_putuns(unsigned int i, int *len);
 int		ft_printf(const char *s, ...);
 
 #endif
